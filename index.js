c86b509aba19cd814d3f56c6ebd5e0770b8f917c
Your Name <you@example.com>
2020-01-29 20:52:29 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    console.log('Welcome to my Page');
+    // const heading = document.createElement("h1");
+    // const heading_text = document.createTextNode("Big Head!");
+    
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Gelacio Azael Fernandez Aldava *Landing Page*
+
+## Structure
+    - header
+    - body
+    - footer
+sources:
+[frontendmasters](https://frontendmasters.github.io/bootcamp/)
